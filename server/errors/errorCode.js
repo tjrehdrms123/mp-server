48141203c6ff63a9caa20b8270f46c64c650df8b
tjrehdrms123 <wwe5455@naver.com>
2022-08-04 11:47:24 +0900

    [ Add ] user signup api

--- server/errors/errorCode.js
+++ server/errors/errorCode.js
@@ -1,10 +1,20 @@
 module.exports = {
-  // 400 Error
+  // 401 Error
+  invalidToken: {
+    status: 401,
+    content: "잘못된 토큰입니다.",
+    errorCode: "MP401000",
+  },
+  notVerifyUser: {
+    status: 401,
+    content: "인증 절차가 이루어지지 않은 유저입니다.",
+    errorCode: "MP401003",
+  },
 
   // 500 Error
   unknownError: {
     status: 500,
     content: "알 수 없는 오류가 발생하였습니다.",
-    errorCode: "999999",
+    errorCode: "MP999999",
   },
 };
