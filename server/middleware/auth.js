86d6c08a458d0bed4a57efb37b7005baa40cceea
tjrehdrms123 <wwe5455@naver.com>
2022-08-05 19:07:50 +0900

    [ Add ] jwt 작업중

---
+++ server/middleware/auth.js
@@ -0,0 +1,17 @@
+const { sign, verify } = require("jsonwebtoken");
+const config = require("../config");
+async function tokenVerify(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    const user = verify(token, config.access_secret);
+    return (data = {
+      user,
+    });
+  } catch (error) {
+    console.log("auth.js error : ", error);
+  }
+}
+
+module.exports = {
+  tokenVerify,
+};
