76c5a79a0f3fbdbcf130cbc81e2731ca74583cf6
tjrehdrms123 <wwe5455@naver.com>
2022-08-07 19:57:14 +0900

    [ Add ] Login, token issuance

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 const config = require("../config");
-const { signUpQuery } = require("../model/user");
+const { signUpQuery, loginQuery } = require("../model/user");
 
 // 회원가입 컨트롤러
 async function signUpController(req, res, next) {
@@ -9,7 +9,7 @@ async function signUpController(req, res, next) {
   // 비밀번호 sha256 알고리즘으로 해시값으로 변경
   const passwordHash = crypto
     .createHmac("sha256", config.SALT)
-    .update(uid + name)
+    .update(password)
     .digest("hex");
 
   const result = await signUpQuery(uid, name, email, passwordHash);
@@ -26,6 +26,27 @@ async function signUpController(req, res, next) {
   }
 }
 
+async function loginController(req, res, next) {
+  const { uid, password } = req.body;
+
+  const passwordHash = crypto
+    .createHmac("sha256", config.SALT)
+    .update(password)
+    .digest("hex");
+
+  const result = await loginQuery(uid, password, passwordHash);
+  if (result.status === 200) {
+    res.status(200).send(result);
+  }
+  if (result.status === 400) {
+    res.status(400).send(result);
+  }
+  if (result.status === 500) {
+    res.status(500).send(result);
+  }
+}
+
 module.exports = {
   signUpController,
+  loginController,
 };
--- server/model/user.js
+++ server/model/user.js
@@ -1,11 +1,23 @@
 const Parse = require("parse/node");
 const config = require("../config");
 const {
+  // 회원가입 에러
   signUpSuccess,
   idDuplicate,
   emailDuplicate,
+  // 로그인 에러
+  idNotfound,
+  passwordNotfound,
+  loginSuccess,
+  infoNotfound,
+  // 그외
   errorCode,
 } = require("../res_code/code");
+
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../middleware/jwt");
 
 Parse.initialize(
   config.parseAppId,
@@ -17,9 +29,12 @@ Parse.User.enableUnsafeCurrentUser();
 
 const User = Parse.Object.extend("user");
 const user = new User();
-// id, email 중복 체크
+// 유저 회원가입 id, email 중복 체크
 const idQry = new Parse.Query(User);
 const emailQry = new Parse.Query(User);
+
+// 유저 로그인
+const userLoginQry = new Parse.Query(User);
 
 // 유저 회원가입
 async function signUpQuery(uid, name, email, passwordHash, verify_type) {
@@ -42,6 +57,7 @@ async function signUpQuery(uid, name, email, passwordHash, verify_type) {
     user.set("password", passwordHash);
     user.set("verify_type", verify_type);
     user.set("delete_status", false);
+    user.set("member_level", 0);
     await user.save();
     signUpSuccess.data.objectId = user.id;
     signUpSuccess.data.uid = uid;
@@ -54,6 +70,37 @@ async function signUpQuery(uid, name, email, passwordHash, verify_type) {
   }
 }
 
+// 유저 로그인
+async function loginQuery(uid, password, passwordHash) {
+  userLoginQry.equalTo("uid", uid);
+  const userLogin = await userLoginQry.first();
+  const userInfo = userLogin?.toJSON();
+  try {
+    if (!uid) {
+      return idNotfound;
+    }
+    if (!password) {
+      return passwordNotfound;
+    }
+    if (uid && password) {
+      if (passwordHash === userInfo?.password) {
+        // DB에 있는 해시 패스워드랑 입력한 비밀번호의 해쉬 값이 같을 경우 토큰 생성
+        let accessToken = generateAccessToken(uid);
+        let refreshToken = generateRefreshToken(passwordHash);
+        loginSuccess.data.accessToken = accessToken;
+        loginSuccess.data.refreshToken = refreshToken;
+        return loginSuccess;
+      } else {
+        return infoNotfound;
+      }
+    }
+  } catch (error) {
+    errorCode.data.content = error.message;
+    return errorCode;
+  }
+}
+
 module.exports = {
   signUpQuery,
+  loginQuery,
 };
