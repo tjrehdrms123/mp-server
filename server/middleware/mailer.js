52eaa2fbcb7c8d03f690d9a84d90fbbb4c690a4a
tjrehdrms123 <wwe5455@naver.com>
2022-08-29 15:32:33 +0900

    [ Add ] config require rm

--- server/middleware/mailer.js
+++ server/middleware/mailer.js
@@ -1,5 +1,4 @@
 const nodemailer = require("nodemailer");
-const config = require("../config");
 
 // async..await is not allowed in global scope, must use a wrapper
 async function mailer(toEmail = "", emailCodeAuthHash) {
