962775252759dc6685b3be06467a5e308a6f2bdc
tjrehdrms123 <wwe5455@naver.com>
2022-09-19 18:52:46 +0900

    [ Add ] Page swagger add

--- server/routes/page.js
+++ server/routes/page.js
@@ -9,29 +9,98 @@ const {
 const router = Router();
 /**
  * @swagger
- *  /auth/email:
- *    post:
+ *  /page:
+ *    get:
  *      tags:
- *      - email
- *      summary: 유저 메일 인증
- *      description: 유저 메일 인증
+ *      - page
+ *      summary: 페이지 전체 가져오기
+ *      description: 페이지 전체 가져오기
  *      consumes:
  *      - application/json
  *      produces:
  *      - application/json
  *      parameters:
- *        - in: body
- *          name: Body
+ *        - in: header
+ *          name: authorization
  *          required: true
- *          description :
- *          schema:
- *              $ref: '#/definitions/AuthEmail'
+ *          description : Bearer AccessToken
  */
-
 router.get("", pageListAllController);
+/**
+ * @swagger
+ *  /page/:id:
+ *    get:
+ *      tags:
+ *      - page
+ *      summary: 한개 페이지 가져오기
+ *      description: 한개 페이지 가져오기
+ *      consumes:
+ *      - application/json
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: header
+ *          name: authorization
+ *          required: true
+ *          description : Bearer AccessToken
+ */
 router.get("/:id", pageListController);
+/**
+ * @swagger
+ *  /page:
+ *    post:
+ *      tags:
+ *      - page
+ *      summary: 페이지 등록하기
+ *      description: 페이지 등록하기
+ *      consumes:
+ *      - application/json
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: header
+ *          name: authorization
+ *          required: true
+ *          description : Bearer AccessToken
+ */
 router.post("", pageCreateController);
+/**
+ * @swagger
+ *  /page/:id:
+ *    patch:
+ *      tags:
+ *      - page
+ *      summary: 페이지 수정하기
+ *      description: 페이지 수정하기
+ *      consumes:
+ *      - application/json
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: header
+ *          name: authorization
+ *          required: true
+ *          description : Bearer AccessToken
+ */
 router.patch("/:id", pageUpdateController);
+/**
+ * @swagger
+ *  /page/:id:
+ *    delete:
+ *      tags:
+ *      - page
+ *      summary: 페이지 삭제하기
+ *      description: 페이지 삭제하기
+ *      consumes:
+ *      - application/json
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: header
+ *          name: authorization
+ *          required: true
+ *          description : Bearer AccessToken
+ */
 router.delete("/:id", pageDeleteController);
 
 module.exports = router;
