162ce4914e894cf227a79c277aa62f857ffa8ba1
tjrehdrms123 <wwe5455@naver.com>
2022-09-28 18:39:25 +0900

    [ Add ] Write page..

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const app = express();
 
 app.use(express.static('public'));
 app.use('/memo', express.static(__dirname + '/public/memo.html'));
+app.use('/memo/write', express.static(__dirname + '/public/memoWrite.html'));
 app.use('/memo/signup', express.static(__dirname + '/public/signup.html'));
 app.use('/memo/email', express.static(__dirname + '/public/email.html'));
 app.use('/memo/login', express.static(__dirname + '/public/login.html'));
